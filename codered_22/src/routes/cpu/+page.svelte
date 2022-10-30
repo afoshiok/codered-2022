9dabdfb3f0dc4c71bc6984b2d3954dac1484eb9e
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-10-30 02:29:31 -0500

    Update +page.svelte

--- codered_22/src/routes/cpu/+page.svelte
+++ codered_22/src/routes/cpu/+page.svelte
@@ -1,37 +1,49 @@
 <h1 class="flex justify-center items-center pt-60 font-bold text-3xl text-pine-green">CPU Carbon Emissions</h1>
 <form>
-    <div class="grid grid-cols-6 grid-rows-3 gap-4">
+    <div class="grid grid-cols-6 grid-rows-2 gap-4">
         <!-- "Region Name" Input -->
         <div class="block col-start-3 col-end-5">
             <label for="base-input" class= "mb-px text-md font-medium text-gray-900 dark:text-gray-300">Region Name </label>
-            <input type="text" id="base-input" class="bg-gray-50 border border-pine-green text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <input type="text" id="emission_factor" name= "emission_factor" class="bg-gray-50 border border-pine-green text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
 
         <!-- Number of CPUs Input -->
         <div class="row-start-2 col-start-3 col-end-4">
             <label for="base-input" class="block mb-px text-sm font-medium text-gray-900 dark:text-gray-300">Number of CPUs</label>
-            <input type="text" id="base-input" class="bg-gray-50 border border-pine-green text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <input type="text" id="num_cpu" name="num_cpu" class="bg-gray-50 border border-pine-green text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
         
         <!-- Percent Load Input -->
         <div class="row-start-2 col-start-4 col-end-6">
             <label for="base-input" class="block mb-px text-sm font-medium text-gray-900 dark:text-gray-300">Percent Load</label>
-            <input type="number" id="base-input" class="bg-gray-50 border border-pine-green text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <input type="number" id="per_load" name="per_load" class="bg-gray-50 border border-pine-green text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </div>
 
         <!-- Serverless? Radio Check -->
         <div class="flex items-end row-start-2 col-start-5 col-end-5 justify-self-start ">
             <label for="checked-toggle" class="inline-flex relative items-center cursor-pointer">
-                <input type="checkbox" value="" id="checked-toggle" class="sr-only peer" checked>
+                <input type="checkbox" value="" id="checked-toggle" class="sr-only peer" checked = {is_checked}>
                 <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-400"></div>
                 <span class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Serverless?</span>
               </label>
         </div>
+    </div>
 
-        <!-- Time Active  Input -->
-        <div class="row-start-3 col-start-3 col-end-5">
-                
+    <!-- Time Active Input -->
+    <div class="grid grid-cols-3">
+        <div class="col-start-2 justify-self-center mt-5">
+            <div>
+                <label for="default-input" class="block mb-px text-sm font-medium text-gray-900 dark:text-gray-300">Time Active (in hours)</label>
+                <input type="text" id="default-input" class="bg-gray-50 border border-pine-green text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/8 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
         </div>
+    </div>
 
+    <div class="flex justify-center mt-4">
+        <button type="submit" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Estimate</button>
     </div>
 </form>
+
+<script lang="ts">
+    const is_checked: boolean = false
+</script>
