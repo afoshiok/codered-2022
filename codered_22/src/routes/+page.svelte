3fdde36a82a705c3b2fcee261e2b88d378ccda72
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-10-29 21:46:56 -0500

    CPU page added

--- codered_22/src/routes/+page.svelte
+++ codered_22/src/routes/+page.svelte
@@ -2,12 +2,12 @@
     <!-- Prompt -->
     <h1 class="pt-60 font-bold text-3xl text-pine-green">Choose Your Cloud Service</h1>
     <!-- Dropdown -->
-    <button id="dropdownDefault" data-dropdown-toggle="dropdown" class="text-lg text-center text-white mt-3.5 w-32 h-.5 bg-green-500 hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">Select <svg class="ml-4 w-3 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+    <button id="dropdownDefault" data-dropdown-toggle="dropdown" class="text-lg text-center text-white mt-3.5 w-32 h-.5 bg-green-500 hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">Select <svg class="ml-4 w-3 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
     <div class="" >
         <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
             <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
             <li>
-                <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">CPU</a>
+                <a href="/cpu" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">CPU</a>
             </li>
             <li>
                 <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Memory</a>
