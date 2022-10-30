59127429d18669a2d2f22b7c9256e360b25c6c33
afoshiok <89757138+afoshiok@users.noreply.github.com>
2022-10-30 11:46:04 -0500

    Update +page.svelte

--- codered_22/src/routes/cpu/+page.svelte
+++ codered_22/src/routes/cpu/+page.svelte
@@ -107,7 +107,7 @@
     </div>
 
     <div class="flex justify-center mt-4">
-        <button type="submit" on:click={handleclick} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Estimate</button>
+        <button type="submit" on:click|once={handleclick} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Estimate</button>
     </div>
 </form>
 
